188b1340c01cb94a51adc8a8faebadf22cdbaa05
Belu Miranda 🌟 <marmiranda@frba.utn.edu.ar>
2024-03-28 10:30:26 -0300

    Update encabezado

---
+++ 00-CHelloWorld/hello.c
@@ -0,0 +1,14 @@
+/* TP 0
+ * TP 0: Consta llevar a cabo las instalaciones necesarias para realizar y compilar un programa en C. En este caso que diga "Hola Mundo"
+ * Autor: Belen Miranda
+ * Cuando: 28703/2024
+ */
+
+#include <stdio.h>
+
+int main()
+{
+    printf( "Hola mundo!" );
+
+    return 0;
+}
