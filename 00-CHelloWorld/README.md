759c250ad299b7897c424dcd9fe366512209c311
Belu Miranda 🌟 <marmiranda@frba.utn.edu.ar>
2024-03-28 09:28:49 -0300

    update encabezado

---
+++ 00-CHelloWorld/README.md
@@ -0,0 +1,9 @@
+# 📌Sintaxis y Semantica de los Lenguajes <img src="https://media.giphy.com/media/XHAv3GveJMXMXSumkO/giphy.gif" width="100" />
+
+------------
+
+- ####  Curso:  K2052
+- #### Año de cursada:  2024
+- #### Legajo:  174.452-5
+- #### Apellido: Miranda
+- #### Nombe: Belén
---
+++ 00-CHelloWorld/hello.c
@@ -0,0 +1,14 @@
+/* TP 0
+ * TP 0: Consta llevar a cabo las instalaciones necesarias para realizar y compilar un programa en C. En este caso que diga "Hola Mundo"
+ * Autor: Belen Miranda
+ * Cuando: Fecha de última modificación
+ */
+
+#include <stdio.h>
+
+int main()
+{
+    printf( "Hola mundo!" );
+
+    return 0;
+}
